6ceb9e83dc76953bb3eb1f5a5796725227785c0e
Bikash Mishra <mishra.bikash002@gmail.com>
2024-05-01 19:52:45 +0530

    add test for in-memory implementation

--- limitless_test.go
+++ limitless_test.go
@@ -1,92 +1,40 @@
 package limitless
 
 import (
-	"context"
-	"fmt"
 	"testing"
 	"time"
-
-	"github.com/redis/go-redis/v9"
 )
 
-func TestInMemoryImpl(t *testing.T) {
-	limiter := NewInMemoryTokenBucket(5, 2)
-	executeExample(t, limiter, 5, 2)
-}
-
-func TestRedisImpl(t *testing.T) {
-	ctx := context.Background()
-	client, err := newRedisClient(ctx)
+func TestInMemoryTokenBucketAllow(t *testing.T) {
+	// Test case: Allow with sufficient tokens
+	tb := NewInMemoryTokenBucket(10, 2)
+	allowed, err := Allow(tb)
 	if err != nil {
-		fmt.Println(err)
-		return
+		t.Errorf("Unexpected error: %v", err)
 	}
-	defer client.Close()
-
-	limiter, err := NewRedisTokenBucket(ctx, client, "myTokenBucket", 5, 2)
-	if err != nil {
-		fmt.Println(err)
-		return
+	if !*allowed {
+		t.Error("Expected to allow with sufficient tokens")
 	}
-	executeExample(t, limiter, 5, 2)
-}
 
-func executeExample(t *testing.T, limiter RateLimiter, capacity int64, rate int) {
-	// rate limit does not exceed
-	failed := false
+	// Test case: Disallow when no tokens are available
 	for i := 0; i < 10; i++ {
-		allow, err := Allow(limiter)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		if allow != nil && *allow {
-			fmt.Printf("Operation allowed")
-		} else {
-			fmt.Printf("Rate limit exceeded")
-			failed = true
-		}
-
-		time.Sleep(time.Duration(rate) * time.Second)
+		_, _ = Allow(tb)
 	}
-	if failed {
-		t.Fail()
-	}
-
-	// rate limit exceeds
-	failed = false
-	for i := 0; i < 10; i++ {
-		allow, err := Allow(limiter)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		if allow != nil && *allow {
-			fmt.Printf("Operation allowed")
-		} else {
-			failed = true
-			fmt.Printf("Rate limit exceeded")
-		}
-
-		time.Sleep(time.Duration(rate-1) * time.Second)
-	}
-	if !failed {
-		t.Fail()
-	}
-}
-
-func newRedisClient(ctx context.Context) (redis.UniversalClient, error) {
-	client := redis.NewUniversalClient(&redis.UniversalOptions{
-		Addrs:    []string{"127.0.0.1:6379"},
-		Password: "XXXX",
-		DB:       0,
-	})
-	_, err := client.Ping(ctx).Result()
+	allowed, err = Allow(tb)
 	if err != nil {
-		return nil, err
+		t.Errorf("Unexpected error: %v", err)
+	}
+	if *allowed {
+		t.Error("Expected to disallow when no tokens are available")
 	}
 
-	return client, nil
+	// Test case: Allow after refilling tokens
+	time.Sleep(5 * time.Second)
+	allowed, err = Allow(tb)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	if !*allowed {
+		t.Error("Expected to allow after refilling tokens")
+	}
 }
